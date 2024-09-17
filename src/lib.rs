69f55144b69ee24c27e71bc7821818e4f28e0585
unknown <yogidwitama56@gmail.com>
2024-09-17 11:59:28 +0700

    update to 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,13 @@ pub fn say_hello(name:&str)->String{
     format!("Hello, {} im 🤡", name)
 }
 
+pub fn say_hello_to_everyone()->String{
+    "Hello, everyone☝️".to_string()
+}
 pub fn  say_goodbye(name:&str)->String{
     format!("Good bye, {}", name)
+}
+
+pub fn say_goodbye_everyone()->String{
+    "Good bye everyone".to_string()
 }
