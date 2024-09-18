96455000280b2033b8a558409367a07a64c410bf
unknown <yogidwitama56@gmail.com>
2024-09-18 09:37:26 +0700

    cfg features

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
+#[cfg(feature = "hello")]
 pub fn say_hello(name:&str)->String{
     format!("Hello, {} im 🤡", name)
 }
-
+#[cfg(feature = "hello")]
 pub fn say_hello_to_everyone()->String{
     "Hello, everyone☝️".to_string()
 }
+#[cfg(feature = "bye")]
 pub fn  say_goodbye(name:&str)->String{
     format!("Good bye, {}", name)
 }
-
+#[cfg(feature = "bye")]
 pub fn say_goodbye_everyone()->String{
     "Good bye everyone".to_string()
 }
